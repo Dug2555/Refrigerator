70638654625305a4a349b6013c464c6ffda850d9
LoweD1 <douglas.lowe@jacobs.com>
2023-12-26 11:08:21 -0600

    Attempted to add write capabilities

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const fs = require("fs");
+
+const example = {
+    id: 2,
+    name: "addtest",
+    date: "5-2022"
+}
+
+const data = JSON.stringify(example);
+
+fs.writeFile("data.json", data, (error) => {
+    // throwing the error
+    // in case of a writing problem
+    if (error) {
+      // logging the error
+      console.error(error);
+  
+      throw error;
+    }
+});
+
 import data from './data.json' assert { type: 'json' };
 console.log(data);
