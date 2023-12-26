ed481fc24989bd3895185cc53db0c9caa25364e3
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-26 13:55:13 -0600

    Commenting out code until website can be transferred

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+//const fs = require("fs");
 
-const example = {
-    id: 2,
-    name: "addtest",
-    date: "5-2022"
-}
+//const example = {
+//    id: 2,
+//    name: "addtest",
+//    date: "5-2022"
+//}
 
-const data = JSON.stringify(example);
+//const data = JSON.stringify(example);
 
-fs.writeFile("data.json", data, (error) => {
-    // throwing the error
-    // in case of a writing problem
-    if (error) {
-      // logging the error
-      console.error(error);
+//fs.writeFile("data.json", data, (error) => {
+//    // throwing the error
+ //   // in case of a writing problem
+ //   if (error) {
+ //     // logging the error
+ //     console.error(error);
   
-      throw error;
-    }
-});
+ //     throw error;
+ //   }
+//});
 
 import filedata from './data.json' assert { type: 'json' };
 console.log(filedata);
