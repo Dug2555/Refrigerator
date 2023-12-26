25cee598f8a95596e8929a191a2cf631fe8c4d77
LoweD1 <douglas.lowe@jacobs.com>
2023-12-26 14:09:22 -0600

    Added geeks for geeks tutorial

--- index.js
+++ index.js
@@ -19,5 +19,31 @@
  //   }
 //});
 
+//added from G4G
+function domReady(fn) { 
+    if ( 
+        document.readyState === "complete" || 
+        document.readyState === "interactive"
+    ) { 
+        setTimeout(fn, 1000); 
+    } else { 
+        document.addEventListener("DOMContentLoaded", fn); 
+    } 
+} 
+  
+domReady(function () { 
+  
+    // If found you qr code 
+    function onScanSuccess(decodeText, decodeResult) { 
+        alert("You Qr is : " + decodeText, decodeResult); 
+    } 
+  
+    let htmlscanner = new Html5QrcodeScanner( 
+        "my-qr-reader", 
+        { fps: 10, qrbos: 250 } 
+    ); 
+    htmlscanner.render(onScanSuccess); 
+});
+
 import filedata from './data.json' assert { type: 'json' };
 console.log(filedata);
